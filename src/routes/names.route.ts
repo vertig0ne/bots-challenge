baceeec8a388972dd30aaf274924626fa21e8c25
Vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2019-12-29 17:02:11 +0000

    rename endpoint to match specification

--- src/routes/names.route.ts
+++ src/routes/names.route.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import controller from "../controller/names.controller";
 
-export const name = "check-names";
+export const name = "name-count";
 export const router = express.Router();
 
 router.get("/", controller.get);
